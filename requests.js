58cfba690f6e514db348e8d3bef69076b3b1b77a
Hou Chia <kchia87@gmail.com>
2021-01-06 19:54:38 -1000

    Update requests.js

--- requests.js
+++ requests.js
@@ -23,3 +23,11 @@ axios
   .then((response) => {
     console.log(response.data);
   });
+
+// Since the ID for the newly created constellation
+// is randomly generated, yours will look different from
+// what is shown here.
+const idToDelete = "ctvVRjy";
+axios.delete(`${url}/${idToDelete}`);
+
+axios.get(`${url}/${idToDelete}`); // should return a 404 error, since the constellation was deleted in the previous API call
